1035c8b1e60c4d861f6accc70e739a9287a7ae68
JAMES ASPINALL <aspin@me.com>
2019-06-04 10:06:57 +1000

    Tried to get Soical Icons to work from FontAwesome plus README

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import SignIn from "../components/signin"
 
+
 export default() => (
 <>
 
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -6,8 +6,8 @@ export default() => (
     <div className = "shop">
         <Layout >
         <div class={styles.shopGridContainer}>
-        <div class={styles.left}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime ad odio necessitatibus saepe adipisci ut, fugiat veritatis sit incidunt exercitationem deleniti accusamus, dignissimos hic aliquid minus. Mollitia quo fugiat corporis?</div>
-        <div class={styles.right}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, accusantium adipisci. Exercitationem sapiente ipsam maiores, ut veniam aspernatur inventore impedit, enim voluptas atque blanditiis, molestias voluptatem corporis? Repellat, ad illum!</div>
+        <div class={styles.left}></div>
+        <div class={styles.right}></div>
         </div>
 
         
