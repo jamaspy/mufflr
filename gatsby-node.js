7c5dc127066c2b2c5a5636f3360f24b9908a6d92
JAMES ASPINALL <aspin@me.com>
2019-06-07 09:17:36 +1000

    Removed CreateBlog from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -55,57 +55,57 @@ exports.createPages = ({ graphql, actions }) => {
     return null
   })
 }
-exports.createCard = ({ graphql, actions }) => {
-  const { createCard } = actions
+// exports.createCard = ({ graphql, actions }) => {
+//   const { createCard } = actions
 
-  const blogPost = path.resolve(`./src/pages/blog.js`)
-  return graphql(
-    `
-      {
-        allContentfulPost {
-          edges {
-            node {
-              title
-              subtitle
-              slug
-              image {
-                fluid {
-                  src
-                }
-              }
-              content {
-                childContentfulRichText {
-                  html
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+//   const blogPost = path.resolve(`./src/pages/blog.js`)
+//   return graphql(
+//     `
+//       {
+//         allContentfulPost {
+//           edges {
+//             node {
+//               title
+//               subtitle
+//               slug
+//               image {
+//                 fluid {
+//                   src
+//                 }
+//               }
+//               content {
+//                 childContentfulRichText {
+//                   html
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   ).then(result => {
+//     if (result.errors) {
+//       throw result.errors
+//     }
 
-    // Create blog posts pages.
-    const posts = result.data.allContentfulPost.edges
+//     // Create blog posts pages.
+//     const posts = result.data.allContentfulPost.edges
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+//     posts.forEach((post, index) => {
+//       const previous = index === posts.length - 1 ? null : posts[index + 1].node
+//       const next = index === 0 ? null : posts[index - 1].node
 
-      createCard({
-        path: post.node.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.slug,
-          previous,
-          next,
-        },
-      })
-    })
+//       createCard({
+//         path: post.node.slug,
+//         component: blogPost,
+//         context: {
+//           slug: post.node.slug,
+//           previous,
+//           next,
+//         },
+//       })
+//     })
 
-    return null
-  })
-}
+//     return null
+//   })
+// }
